e4aaea26d2b0b17fec221c57495a39869c4c81b9
Princy Wamal <princywamal@gmail.com>
2025-04-09 11:31:49 +0200

    Configuration of the chatbot controller

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -3,5 +3,19 @@ class ChatbotController < ApplicationController
   end
 
   def ask
+    user_message = params[:message].downcase
+
+    # Looking for matchings (this is a simple way, we can also find matchings with the answer and the category)
+    @question = Question.all.find do |q|
+      user_message.include?(q.query.downcase) ||
+      q.query.downcase.include?(user_message)
+    end
+    if @question
+      response = @question.answer
+    else
+      response = "Désolé, je n'ai pas la réponse à ta question. Peux-tu reformuler ou poser une question ?"
+    end
+
+    render json: { response: response }
   end
 end
